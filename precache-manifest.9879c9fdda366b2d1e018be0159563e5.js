bcd11b20910c1bdffca30ece1f9f234be2ce00f8
Mykola Chyrkov <nikolay4irkov@gmail.com>
2019-07-22 16:54:21 +0300

    Updates

---
+++ precache-manifest.9879c9fdda366b2d1e018be0159563e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a09e7a98c07fdf0f7dac3cd42175e0",
+    "url": "/forms/index.html"
+  },
+  {
+    "revision": "40855746bc3500e465a5",
+    "url": "/forms/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6873e5317d425eba7651",
+    "url": "/forms/static/js/2.46439f22.chunk.js"
+  },
+  {
+    "revision": "40855746bc3500e465a5",
+    "url": "/forms/static/js/main.3a59ff9e.chunk.js"
+  },
+  {
+    "revision": "7b433bcb0485557f4f92",
+    "url": "/forms/static/js/runtime~main.93bf94d6.js"
+  }
+]);
